98e63100fbaddb0aee6dc31ee76fb60b0428702b
Konovalovlev <Konovalovlev7777@gmail.com>
2025-01-27 21:57:37 +0300

    krutyak
    koniks

---
+++ zadanie_3_/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class SadKitten
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("    /\\_/\\");
+        Console.WriteLine("   ( o.o )");
+        Console.WriteLine("    > ^ <   ");
+        Console.WriteLine("   /    \\ ");
+        Console.WriteLine("  /______\\ ");
+        Console.WriteLine("  (______)");
+        Console.WriteLine("  (   _  )");
+        Console.WriteLine("   \\____/  ");
+        Console.WriteLine();
+        Console.WriteLine("Грустный котенок :(");
+        Console.WriteLine("Я непонял как делать задание оно находится в другой папке пощадите котёнка");
+        Console.WriteLine("Желаю доброго урта :D");
+    }
+}
---
+++ Извеняюсь/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_3_
+{
+
+    abstract public class Apptication
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            }
+        }
+
+        class WinFactory : GUIFactory
+        {
+            public Button CreateButton()
+            {
+                Console.WriteLine("Создалась win кнопка");
+                return new WinButton();
+            }
+            public Checkbox CreateCheckbox()
+            {
+                Console.WriteLine("Закрылась win кнопка");
+                return new WinCheckbox();
+            }
+        }
+        class MacFactory : GUIFactory
+        {
+            public Button CreateButton()
+            {
+                Console.WriteLine("Создалась mac кнопка");
+                return new MacButton();
+            }
+            public Checkbox CreateCheckbox()
+            {
+                Console.WriteLine("Закрылась mac кнопка");
+                return new MacCheckbox();
+            }
+        }
+        public interface GUIFactory
+        {
+            Button CreateButton();
+            Checkbox CreateCheckbox();
+        }
+        public class Button
+        {
+        }
+        public class Checkbox
+        {
+        }
+        public class WinCheckbox : Checkbox
+        {
+        }
+        public class WinButton : Button
+        {
+        }
+        public class MacButton : Button
+        {
+        }
+        public class MacCheckbox : Checkbox
+        {
+
+        }
+    }
+    
+}
+
